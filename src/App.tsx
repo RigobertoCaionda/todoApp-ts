f5cafdd5beb3065a82f4a5307995fc8d6a9f8622
Rigoberto Caionda <rigobertocaionda98@gmail.com>
2021-10-16 22:48:13 +0100

    Creating a todo app with reactJS and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import * as C from './App.styles';
+import {useState} from 'react';
+import {Item} from './types/item';
+import {ListItem} from './components/ListItem';
+import {AddArea} from './components/AddArea';
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    {id: 1, name: 'Comprar o pao na padaria', done: false},
+    {id: 2, name: 'Comprar um bolo na padaria', done: true}
+  ]);//Quer dizer que o tipo desse useState e o Item, e um array de Item (Isso e TS)
+  const handleAddTask = (taskName: string) => {
+    let newList = [...list];
+    newList.push({
+      id: list.length + 1,
+      name: taskName,
+      done: false
+    });
+    setList(newList);
+  }
+  const handleTaskChange = (id: number, done: boolean) => {
+    let newList = [...list];
+    for(let i in newList) {
+      if(newList[i].id === id) {
+        newList[i].done = done;
+      }
+    }
+    setList(newList);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <C.Container>
+       <C.Area>
+         <C.Header>Lista de tarefas</C.Header>
+         <AddArea onEnter={handleAddTask}/>
+         {list.map((item, index)=>(
+            <ListItem item={item} key={index} onChange={handleTaskChange}/>
+          ))}
+       </C.Area>
+    </C.Container>
+    );
 }
-
 export default App;
---
+++ src/components/ListItem/index.tsx
@@ -0,0 +1,15 @@
+import * as C from './styled';
+import {Item} from '../../types/item';
+type Props = {
+	item: Item,
+	onChange: (id: number, done: boolean) => void
+}
+
+export const ListItem = ({item, onChange}: Props) => {
+	return (
+			<C.Container done={item.done}>
+				<input type="checkbox" checked={item.done} onChange={e => onChange(item.id, e.target.checked)}/>
+				<label>{item.name} - {item.done.toString()}</label>
+			</C.Container>
+		);
+}
